5b250a02c2893335f5301e80a23f702f508b511d
Jacopo Cascioli <jacopocascioli@gmail.com>
2018-03-14 19:55:27 +0000

    feat(cli): add cli file

---
+++ cli.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+import os
+import sys
+import tweepy
+import click
+
+
+class Twitter:
+
+    def __init__(self):
+        consumer_secret = os.getenv('CONSUMER_SECRET')
+        consumer_key = os.getenv('CONSUMER_KEY')
+        access_token = os.getenv('ACCESS_TOKEN')
+        access_token_secret = os.getenv('ACESS_TOKEN_SECRET')
+        auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+        auth.set_access_token(access_token, access_token_secret)
+        self.api = tweepy.API(auth)
+
+
+class Cli:
+
+    @click.group()
+    def main():
+        pass
+
+    @main.command()
+    @click.argument('content')
+    def tweet(content):
+        twitter = Twitter()
+        twitter.api.update_status(content)
+
+    @main.command()
+    @click.argument('who')
+    def follow(who):
+        twitter = Twitter()
+        twitter.api.create_friendship(who)
+
+
+if __name__ == '__main__':
+    Cli.main()
